40aab1f4359d20b2805afe764bdf5205d6f08bea
Lucas Denton <ladent99@gmail.com>
2025-05-07 14:00:01 -0400

    init

---
+++ configuration.nix
@@ -0,0 +1,61 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./users-and-groups.nix
+      ./system-configuration.nix
+      ./graphics-configuration.nix
+    ];
+
+
+
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  git
+  wget
+  mangohud
+  protonup
+  protonup-qt
+  bitwarden-desktop
+  discord
+  bottles
+  lutris
+  vscodium
+  calibre
+  kubectl
+  tidal-hifi
+  heroic
+  winetricks
+  ];
+
+  environment.variables.EDITOR = "vim";
+  
+  environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\\\${HOME}/.steam/root/compatibilitytools.d";
+    MOZ_ENABLE_WAYLAND = 1;
+    NV_PRIME_RENDER_OFFLOAD = 1;
+    NV_PRIME_RENDER_OFFLOAD_PROVIDER= "NVIDIA-G0";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    CLUTTER_DEFAULT_FPS = 165;
+    __GL_SYNC_DISPLAY_DEVICE = "DP-1";
+  };
+
+
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.firefox.enable = true;
+  programs.gamemode.enable = true;
+
+
+}
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Gaming PC";
+
+  inputs = {
+    # NixOS official package source, using the nixos-24.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.LD-NixOS-PC = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
