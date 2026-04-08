c3c0faf712a5f79ba4107fd39b978a42c6e7d156
Lucas Denton <ladent99@gmail.com>
2026-04-08 15:41:22 -0400

    ollama cached

--- LD-NixOS-PC/default.nix
+++ LD-NixOS-PC/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ollama-cache, ... }:
 
 {
   imports =
--- flake.nix
+++ flake.nix
@@ -1,28 +1,44 @@
 {
   description = "Nix Configs";
 
+  nixConfig = {
+    ## Caching for long running binary builds
+    extra-substituters = [
+      "https://ladent.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "ladent.cachix.org-1:iJiLXkTbnPWmZ0OWqt9M4zFipIRdStMR1fF1eJpsI8k="
+    ];
+  };
   inputs = {
     # NixOS official package source, using unstable 
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    ollama-cache.url = "github:ladent99/ollama-binary-caching";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, ollama-cache, ... }@inputs: 
+  
+      
+  {
 
     nixosConfigurations = {
 
       # Setup Lucas' PC
       LD-NixOS-PC = let 
         hostName = "LD-NixOS-PC";
+        system = "x86_64-linux";
         specialArgs = {
           inherit hostName;
+          inherit ollama-cache;
           };
+
       in nixpkgs.lib.nixosSystem {
         inherit specialArgs;
-        system = "x86_64-linux";
 
         modules = [
           ./general-config # Import general modules
           ./nvidia-graphics # Import nvidia graphics
+          ./modules/ollama-overlay.nix
           ./${hostName} # Import host specific config
         ];
 
